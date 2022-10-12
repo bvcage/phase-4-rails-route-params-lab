5b2e7796a37a9fb153e40a682503e61a1a4863e2
Bailey Cage <bailey.cage@gmail.com>
2022-10-12 12:13:45 -0400

    commit tree a42fdf906bea4cfe23a8801d080b91891a9da445

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name]
+      first_name_matches = students.where("first_name LIKE ?", "%#{params[:name]}%")
+      last_name_matches = students.where("last_name LIKE ?", "%#{params[:name]}%")
+      students = first_name_matches + last_name_matches
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
